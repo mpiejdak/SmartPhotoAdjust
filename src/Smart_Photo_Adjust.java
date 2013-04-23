e0b971cc5c262e196627e4438d12523166acc25b
Aleksander Stanczak <stanczak.aleksander@gmail.com>
2013-04-23 18:50:01 +0200

    Application skeleton.

--- src/Smart_Photo_Adjust.java
+++ src/Smart_Photo_Adjust.java
@@ -10,7 +10,35 @@ import ij.plugin.frame.*;
 public class Smart_Photo_Adjust implements PlugIn {
 
 	public void run(String arg) {
-		IJ.showMessage("My_Plugin","Hello world!\n"+Perceptron.returnInt());
+		
+		// variables
+		int[] inputVector = null;
+		int[] outputVector = null;
+		
+		/////
+		// find inputVector - information about unprocessed image 
+		
+		// TO DO
+		
+		/////
+		
+		/////
+		// process by neural network and suggest adjustments (as outputVector)
+		
+		
+		Perceptron p = new Perceptron();
+		//outputVector = p.getNetworkOutput(inputVector);
+		/////
+		
+		/////
+		// adjust image
+		
+		// TO DO
+		
+		/////
+		
+		IJ.showMessage("Smart Photo Adjust Plugin","Automatic photo adjust conducted succesfully!");
+		
 	}
 	
 	
